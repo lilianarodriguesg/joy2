ed48a300aa5df682ea0a5f40bf3959c122380fbb
liliana rodrigues <lilianarodrigues24.04.1980@gmail.com>
2014-10-15 12:02:33 +0100

    4 botoes,simbolos limites

--- js/main.js
+++ js/main.js
@@ -1,19 +1,43 @@
 $(document).ready(function(){
 
-	$('#bt2').click(function(){
-	$b =$( "#boneco");
-	position=$b.position();
-	newpleft=position.left+10;
-	$b.css('left',newpleft+'px');
-	})
-
-
 	$('#bt1').click(function(){
 	$b =$( "#boneco");
 	position=$b.position();
-	newpleft=position.left-10;
+	newpleft=position.left+10;
+	if (newpleft >900) {
+	newpleft =900;
+	}
 	$b.css('left',newpleft+'px');
 	})
 
 
-});
+	$('#bt2').click(function(){
+	$b =$( "#boneco");
+	position=$b.position();
+	newpleft = position.left-30;
+	 if (newpleft <40){
+	 newleft=40;
+	 }
+	$b.css('left',newpleft+'px');
+	})
+
+	$('#bt3').click(function(){
+	$b =$( "#boneco");
+	position=$b.position();
+	newpleft=position.top-30;
+	if(newpleft < 40){
+	newpleft=40;
+	}
+	$b.css('top',newpleft+'px');
+	})
+
+	$('#bt4').click(function(){
+	$b =$( "#boneco");
+	position=$b.position();
+	newpleft=position.top+30;
+	if(newpleft > 250){
+	newpleft= 250;
+	}
+	$b.css('top',newpleft+'px');
+	})
+	});
